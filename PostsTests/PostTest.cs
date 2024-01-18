067ce6bfcf5cc93d58866a1c63f63b8e8b61676c
Jose A Flores <jose_a_flores@claro.com.do>
2024-01-18 16:21:21 -0400

    save changes

--- PostsTests/PostTest.cs
+++ PostsTests/PostTest.cs
@@ -3,8 +3,43 @@ namespace PostsTests
     public class PostTest
     {
         /*
-         
+         1. with bdChecker null throw argument null exception
+          
          
          */
     }
 }
+
+
+/*
+        public class PostService{
+
+        Atributtes
+            BadWordsChecker
+            PostRepository
+        Methods
+            PostService(BadWordsChecker bdChecker,PostRepository postRepository)
+            int CreatePost(Post post)
+        }
+        ```
+
+        ```
+        public class BadWordsChecker{
+
+        Atributtes
+            BadWordsGlobalList
+        Methods
+            BadWordsChecker(BadWordsGlobalList bwList)
+            List<string> CheckerText(string post)
+        }
+        ```
+
+        ```
+        public class PostRepository{
+
+        Atributtes
+            APIUrl
+        Methods
+            int SendPost(Post post)
+        }
+ */
