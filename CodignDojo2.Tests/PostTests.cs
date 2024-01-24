0d8cff4d3a0cb8c076a9f5696647466b1c2cf04a
Marlon <iamvillalona@gmail.com>
2024-01-24 16:56:58 -0400

    my commmit

--- CodignDojo2.Tests/PostTests.cs
+++ CodignDojo2.Tests/PostTests.cs
@@ -1,4 +1,5 @@
 using CodigoDojo2;
+using CodigoDojo2.Interfaces;
 
 namespace CodignDojo2.Tests
 {
@@ -13,7 +14,9 @@ namespace CodignDojo2.Tests
         public void WhenReceiveGoodPostReturnId()
         {
             Post newP = new Post();
-
+            //  newP.Title = new Mock<IPostRepository>();
+            newP.Title = "Titulo de prueba";
+            newP.Body = "Esto es un body de prueba";
            
         }
     }
