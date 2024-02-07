a2f965536240f93e50f93cf6f8978bfce10621b9
CODETEL\Josue Pujols <josue_pujols@claro.com.do>
2024-02-07 11:26:19 -0400

    added unit test

--- Kata06-Anagrams.Test/AnagramsTests.cs
+++ Kata06-Anagrams.Test/AnagramsTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Kata06_Anagrams;
+using System.Collections.Generic;
 
 namespace Kata06_Anagrams.Test
 {
@@ -10,9 +11,29 @@ namespace Kata06_Anagrams.Test
         [TestMethod]
         public async void When_PassedValidAnagrams_GetAnagrams_ShouldReturnAnagrams()
         {
-          
+            // Arrange
+            var wordlist = new List<string> { "act", "cat", "dog", "god", "tea", "ate", "eat" };
+            var expectedAnagrams = new List<string[]>
+            {
+                new string[] { "act", "cat" },
+                new string[] { "dog", "god" },
+                new string[] { "ate", "eat", "tea" }
+                // Add more expected anagrams if needed
+            };
 
-            await Anagrams.GetAnagrams();
+            // Act
+            var actualAnagrams = Anagrams.GetAnagrams(wordlist);
+
+            // Assert
+            Assert
+            Assert.NotNull(actualAnagrams);
+
+            Assert.Equal(expectedAnagrams.Count, actualAnagrams.Count());
+
+            foreach (var expectedAnagram in expectedAnagrams)
+            {
+                Assert.Contains(expectedAnagram.ToHashSet(), actualAnagrams.Select(a => a.ToHashSet()));
+            }
         }
     }
 }
