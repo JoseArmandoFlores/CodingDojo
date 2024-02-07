5a4e69f3b39f2c60269f72218076dee4fff8235a
Jose Rafael Cruz <jcruz@sweatworks.net>
2024-02-07 12:04:47 -0400

    Refactor: code errors

--- Anagram/AnagramApp/Program.cs
+++ Anagram/AnagramApp/Program.cs
@@ -1,12 +1,19 @@
 ﻿using AnagramApp.Services;
+using AnagramApp.Services.Abstractions;
+using System.Diagnostics;
 
+Stopwatch timer = new Stopwatch();
+timer.Start();
 Console.WriteLine("Anagram Word");
-
-List<string> words = await FileReader.GetWords();
+IAlgorithmService algorithmService = new AlgorithmService();
+IWordService WordService = new WordService();
+List<string> words = await WordService.GetWords();
 int counter = 0;
-List<string[]> anagramsWords = AnagramAlgorithm.FindAnagrams(words).ToList();
+List<string[]> anagramsWords = algorithmService.FindAnagrams(words).ToList();
+timer.Stop();
 foreach (string[] anagrams in anagramsWords)
 {
     counter++;
     Console.WriteLine($"{counter}: {string.Join(" ", anagrams)}");
 }
+Console.WriteLine($"");
