ba56296951201c9b9f73d0a4f583c4b1cf3dc14f
CODETEL\Josue Pujols <josue_pujols@claro.com.do>
2024-02-07 10:57:16 -0400

    initial commit

--- Kata06-Anagrams/Program.cs
+++ Kata06-Anagrams/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int counter = 0;
+var data = (await File.ReadAllLinesAsync(@"wordlist.txt")).ToList();
+var words = data
+        ?.GroupBy(word => string.Concat(word.OrderBy(c => c)))
+        .Where(group => group.Count() > 1)
+        .Select(group => group.OrderBy(word => word).ToArray())
+        ?? throw new ArgumentNullException(nameof(data));
+
+
+foreach (var anagram in words)
+{
+    counter++;
+    Console.WriteLine($"Cantidad: {counter} Resultado: {string.Join(" ", anagram)}");
+}
+
