a10f34099898aa860b031ed21610706fc5100d80
Jose Rafael Cruz <jcruz@sweatworks.net>
2024-02-07 11:08:51 -0400

    Feat: Add initial project

---
+++ Anagram/AnagramApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Anagram;
+
+Console.WriteLine("Anagram Word");
+
+List<string> words = await FileReader.GetWords();
+int counter = 0;
+List<string[]> anagramsWords = Algorithm.FindAnagrams(words).ToList();
+foreach (string[] anagrams in anagramsWords)
+{
+    counter++;
+    Console.WriteLine($"{counter}: {string.Join(" ", anagrams)}");
+}
---
+++ Anagram/AnagramApp/Services/FileReader.cs
@@ -0,0 +1,8 @@
+﻿namespace Anagram
+{
+    public class FileReader
+    {
+        public async static Task<List<string>> GetWords()
+            => (await File.ReadAllLinesAsync(@"words.txt")).ToList();
+    }
+}
